e3cb7c2856197786a1debd5935078c3cea815eb7
Mayaki <ujehmayaki@gmail.com>
2025-08-12 22:29:27 +0100

    broke test 2

--- test_app.py
+++ test_app.py
@@ -52,7 +52,7 @@ class FlaskAuthTestCase(unittest.TestCase):
         })
         self.assertIn(b"Username 'nouser' not found", response.data)
 
-    def test_invalid_action(self):
+    de test_invalid_action(self):
         response = self.app.post('/login', data={
             'username': 'anyuser',
             'password': 'anypass',
@@ -60,5 +60,5 @@ class FlaskAuthTestCase(unittest.TestCase):
         })
         self.assertIn(b'Unknown action.', response.data)
 
-if __nam__ == '__main__':
+if __name__ == '__main__':
     unittest.main()
